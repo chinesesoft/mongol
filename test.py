b187798f406a9c2ced49c397746decb12bdfede7
mothra <mothra@ks3098616.kimsufi.com>
2012-10-11 20:01:36 +0200

    working ackattack in test.py

--- test.py
+++ test.py
@@ -1,21 +1,20 @@
-import time as timefuck
 from scapy.all import *
 import socket
 import logging
 
-# Change log level to suppress annoying IPv6 error
-import logging
-logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
-
 def ackattack(host):
+	loggin.getLogger("scapy.runtime").setLevel(logging.ERROR)
 	ipaddr = socket.gethostbyname(host)
 	port = RandNum(1024,65535)
 
-        for time in range(0,255):
-                ack = IP(dst=ipaddr, ttl=time)/TCP(sport=port, dport=80, flags="A")
-                send(ack)
-		
-		# Prevent flood detection
-		timefuck.sleep(0.1)
+        ack = IP(dst=ipaddr, ttl=(1,255))/TCP(sport=port, dport=80, flags="A")
+        ans,unans = sr(ack, timeout=4)
+	
+	for snd,rcv in ans:
+		endpoint = isinstance(rcv.payload, TCP)
+		print snd.ttl, rcv.src, endpoint
+		if endpoint:
+			break
+
 
 ackattack("thinkshop.cn")
