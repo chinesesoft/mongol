793ba698d921c4a93d423e2c5b843a12266b1d54
mothra <mothra@ks3098616.kimsufi.com>
2012-10-12 07:37:21 +0200

    Stable working firewall finder

--- mongol.py
+++ mongol.py
@@ -2,27 +2,37 @@
 
 import socket
 import logging
+import time
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
+debug = True
+
+# Basically a slightly modified traceroute
 def ackattack(host):
         port = RandNum(1024,65535)
 
         ack = IP(dst=host, ttl=(1,255))/TCP(sport=port, dport=80, flags="A")
         ans,unans = sr(ack, timeout=4)
 
+	iplist = []
 	retdata = ""
         for snd,rcv in ans:
-		print rcv.summary()
+		#print rcv.summary()
                 endpoint = isinstance(rcv.payload, TCP)
                 retdata += "%s %s %s\n" % (snd.ttl,rcv.src,endpoint)
-                if endpoint:
+                iplist.append(rcv.src)
+
+		if endpoint:
                         break
-	return retdata
+	
+	return retdata, iplist
 
 
 MESSAGE = "GET %s HTTP/1.1" + "\x0d\x0a" + "Host: %s" + "\x0d\x0a\x0d\x0a"
-hostnames = ["thinkshop.cn", "d.cn"]
+#hostnames = ["thinkshop.cn"]
+#hostnames = ["www.zju.edu.cn"]
+hostnames = ["pku.edu.cn"]
 port = 80
 
 for host in hostnames:
@@ -53,27 +63,59 @@ for host in hostnames:
 		print "RST: Possibly already blocked"
 		continue
 
+	s.close()
+
 	if response.find("200 OK") != -1:
 		# http://en.wikipedia.org/wiki/List_of_blacklisted_keywords_in_the_People%27s_Republic_of_China
 		# tibetalk
-		traceNoFW = ackattack(ipaddr)
+		noFWprint, noFWlist = ackattack(ipaddr)
 
 		# possibly a delay from the IDS to reaction time
-		time.sleep(1)
 
 		print "Sending stimulus"		
+		
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		try:
+ 	               s.connect((ipaddr, port))
+ 	        except socket.timeout:
+         	       	print "connection to " + host + " has timedout moving on"
+			continue
+
 		s.send(MESSAGE % ("/tibetalk", host) )
+
+		# possibly a delay from the IDS to reaction time
+		time.sleep(3)
 
 		try:
 			response = s.recv(1024)
 		except socket.error:
 			print "Found a filter\n\n"
-			traceFW = ackattack(ipaddr)
-			print "\n\nIPADDR: " + ipaddr
-			print "Without FW:\n\n"
-			print traceNoFW
-			print "\n\nWith FW:\n\n"
-                	print traceFW
+			FWprint, FWlist = ackattack(ipaddr)
+
+			if debug:
+				print "\n\nIPADDR: " + ipaddr
+				print "Without FW:"
+				print noFWprint
+				print "\n\nWith FW:"
+                		print FWprint
+			
+			filterIP = FWlist[-2]
+			# we only check the first 3 octecs because of variation in the routers depending on
+			# firewall status
+			
+			# fuck regex's
+			shortip = filterIP.split(".")
+			shortip = "%s.%s.%s." % (shortip[0], shortip[1], shortip[2])
+			print "shortip: " + shortip
+
+			if shortip in noFWlist:
+				hopsdiff = noFWlist.index(filterIP) - FWlist.index(filterIP)
+				print "Guess [strong]: " + filterIP
+				print "IP block: " + shortip
+				print "Hops diff:    " + str(hopsdiff)
+			else:
+				print "Guess [weak]: " + filterIP
+
 		else:
 			#print response
 			print "Appears not to be blocking"
--- test.py
+++ test.py
@@ -3,29 +3,18 @@ import socket
 import logging
 
 def ackattack(host):
-	loggin.getLogger("scapy.runtime").setLevel(logging.ERROR)
+	logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 	ipaddr = socket.gethostbyname(host)
 	port = RandNum(1024,65535)
 
-        ack = IP(dst=ipaddr, ttl=(1,255))/TCP(sport=port, dport=80, flags="A")
+        ack = IP(dst=ipaddr, ttl=(1,145))/TCP(sport=port, dport=80, flags="A")
         ans,unans = sr(ack, timeout=4)
 	
 	for snd,rcv in ans:
+		print rcv.summary()
 		endpoint = isinstance(rcv.payload, TCP)
 		print snd.ttl, rcv.src, endpoint
 		if endpoint:
 			break
 
-host = "thinkshop.cn"
-port = 80
-MESSAGE = "GET %s HTTP/1.1" + "\x0d\x0a" + "Host: %s" + "\x0d\x0a\x0d\x0a"
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(5)
-try:
-	s.connect((host, port))
-except socket.timeout:
-        print "connection to " + host + " has timedout moving on"
-s.send(MESSAGE % ("/", host))
-response = s.recv(1024)
-print response
+ackattack("thinkshop.cn")
